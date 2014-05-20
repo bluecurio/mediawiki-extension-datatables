8244b79a2f3b0a780cf7019525e08cd70c030e66
DanielRenfro <bluecurio@gmail.com>
2014-05-20 18:52:08 -0400

    Updating extension

--- DataTables.php
+++ DataTables.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This extension adds the jQuery DataTables extension to Mediawiki. 
+ * This extension adds the jQuery DataTables extension to Mediawiki.
  *
  * Author:
  *   Mediawiki extension    Daniel Renfro [ drenfro@vistaprint.net ]
@@ -20,8 +20,8 @@ EOT;
 
 
 // ============= default global variables, constants, etc. =====================
-define( 'MW_DATATABLES_VERSION', '1.1' );
-define( 'DATATABLES_VERSION', '1.9.4' );
+define( 'MW_DATATABLES_VERSION', '1.2' );
+define( 'DATATABLES_VERSION', '1.10.0' );
 
 // ============ credits =================================
 $wgExtensionCredits['other'][] = array(
@@ -29,7 +29,7 @@ $wgExtensionCredits['other'][] = array(
 	'description' 	=> 'Datatables jQuery library for MediaWiki',
 	'author' 		=> array(
 		'[[User:Drenfro|Daniel Renfro]] (Mediawiki Extension)',
-		'Allan Jardine (Javascript library)'
+		'Allan Jardine (Javascript/jQuery library)'
 	),
 	'version' 		=> MW_DATATABLES_VERSION
 );
@@ -43,10 +43,10 @@ $wgHooks['SoftwareInfo'][] = 'wfDataTablesExtensionInfo';
 
 
 /**
- * Adds the version number of Allan Jardine's jQuery extension "DataTables" to 
+ * Adds the version number of Allan Jardine's jQuery extension "DataTables" to
  * the 'Installed Software' section of Special:Version.
- * 
- * @param mixed $softwareInfo 
+ *
+ * @param mixed $softwareInfo
  * @access public
  * @return true
  */
@@ -57,10 +57,10 @@ function wfDataTablesExtensionInfo( &$softwareInfo ) {
 
 
 /**
- * Basic class for registering the resources of Datatables (JS/CSS) with the 
+ * Basic class for registering the resources of Datatables (JS/CSS) with the
  * resource loader.
  *
- * If you'd like to use datatables with your extension, you'll need to list it as a 
+ * If you'd like to use datatables with your extension, you'll need to list it as a
  * dependency when registering your extenion's resources with the ResourceLoader.
  * This would be one of the 'ext.*' values below. For example
  *
@@ -73,63 +73,73 @@ function wfDataTablesExtensionInfo( &$softwareInfo ) {
 class DataTables {
 
     static public function getModules() {
-		$path = 'DataTables/';
+		$path = 'DataTables-1.10.0/';
+
         return array(
             'ext.datatables' => array(    // the main jQuery.datatable() functionality
                 'scripts' => $path . 'media/js/jquery.dataTables.js',
                 'styles' => $path . 'media/css/jquery.dataTables.css'
             ),
             'ext.datatables.autofill' => array(
-                'scripts' => $path . 'extras/AutoFill/media/js/AutoFill.js',
-                'styles' => $path . 'extras/AutoFill/media/css/AutoFill.css',
+                'scripts' => $path . 'extensions/AutoFill/js/dataTables.autoFill.js',
+                'styles' => $path . 'extensions/AutoFill/css/dataTables.autoFill.css',
                 'dependencies' => array( 'ext.datatables' )
             ),
             'ext.datatables.colreorder' => array(
-                'scripts' => $path . 'extras/ColReorder/media/js/ColReorder.js',
-                'styles' => $path . 'extras/ColReorder/media/css/ColReorder.css',
+                'scripts' => $path . 'extensions/ColReorder/js/dataTables.colReorder.js',
+                'styles' => $path . 'extensions/ColReorder/css/dataTables.colReorder.css',
                 'dependencies' => array( 'ext.datatables' )
             ),
             'ext.datatables.colvis' => array(
-                'scripts' => $path . 'extras/ColVis/media/js/ColVis.js',
-                'styles' => $path . 'extras/ColVis/media/css/ColVis.css',
+                'scripts' => $path . 'extensions/ColVis/js/dataTables.colVis.js',
+                'styles' => $path . 'extensions/ColVis/css/dataTables.colVis.css',
                 'dependencies' => array( 'ext.datatables' )
             ),
             'ext.datatables.fixedcolumns' => array(
-                'scripts' => $path . 'extras/FixedColumns/media/js/FixedColumns.js',
+				'scripts' => $path . 'extensions/FixedColumns/js/dataTables.fixedColumns.js',
+				'styles' => $path . 'extensions/FixedColumns/css/dataTables.fixedColumns.css',
                 'dependencies' => array( 'ext.datatables' )
             ),
             'ext.datatables.fixedheader' => array(
-                'scripts' => $path . 'extras/FixedHeader/js/FixedHeader.js',
+				'scripts' => $path . 'extensions/FixedHeader/js/dataTables.fixedHeader.js',
+				'styles' => $path . 'extensions/FixedHeader/css/dataTables.fixedHeader.css',
                 'dependencies' => array( 'ext.datatables' )
             ),
             'ext.datatables.keytable' => array(
-                'scripts' => $path . 'extras/KeyTable/js/KeyTable.js',
+				'scripts' => $path . 'extensions/KeyTable/js/dataTables.keyTable.js',
+				'styles' => $path . 'extensions/KeyTable/css/dataTables.keyTable.css',
                 'dependencies' => array( 'ext.datatables' )
             ),
             'ext.datatables.scroller' => array(
-                'scripts' => $path . 'extras/Scroller/media/js/Scroller.js',
+				'scripts' => $path . 'extensions/Scroller/js/dataTables.scroller.js',
+				'styles' => $path . 'extensions/Scroller/css/dataTables.scroller.css',
                 'dependencies' => array( 'ext.datatables' )
-            ),
+			),
+
+			/**
+			 * Never could get this to work right based on the .swf dependency
+			 *
             'ext.datatables.tabletools' => array(
-                'scripts' => $path . 'extras/TableTools/media/js/TableTools.js',
-                'styles' => $path . 'extras/TableTools/media/css/TableTools.css',
+                'scripts' => $path . 'extensions/TableTools/media/js/TableTools.js',
+                'styles' => $path . 'extensions/TableTools/media/css/TableTools.css',
                 'dependencies' => array( 'ext.datatables' )
-            ),
+			),
+			 */
         );
     }
 
 	public static function register( $resourceLoader ) {
 		global $wgExtensionAssetsPath;
-		
+
 		$localpath = dirname( __FILE__ );
 		$remotepath = "$wgExtensionAssetsPath/DataTables";
-		
+
 		foreach ( self::getModules() as $name => $resources ) {
-			$resourceLoader->register( 
+			$resourceLoader->register(
 				$name, new ResourceLoaderFileModule( $resources, $localpath, $remotepath )
 			);
 		}
-		
+
 		return true;
 	}
 }
